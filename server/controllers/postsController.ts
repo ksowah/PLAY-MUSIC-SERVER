0f9540fb615905d25d00a5c10300a9d76dae43cc
ksowah <sowahkelvin640@gmail.com>
2022-07-26 23:32:38 +0000

    add methods to post controllers

--- server/controllers/postsController.ts
+++ server/controllers/postsController.ts
@@ -6,11 +6,44 @@ export const getPosts = async (req: Request, res: Response) => {
     try {
         const postMessages = await PostMessage.find()
         res.status(200).json(postMessages)
-    } catch (error) {
-        
+    } catch (error: any) {
+        res.status(404).json({message: error.message})
     }
 }
 
-export const createPost = (req: Request, res: Response) => {
-    res.send("Post Creation")
+export const createPost = async (req: Request, res: Response) => {
+
+    try {
+        const post = await PostMessage.create(req.body)
+        res.status(201).json(post)
+   } catch (error) {
+    res.status(409).json({message: error})
+   }
+}
+
+export const updatePost = async (req: Request, res: Response) => {
+    const post = await PostMessage.findById(req.params.postID)
+
+    if(!post){
+        res.status(400)
+        throw new Error("Whoops!! couldn't find post in our database")
+    }
+
+    const updatePost = await PostMessage.findByIdAndUpdate(req.params.postID, req.body, { new: true })
+
+    res.status(200).json(updatePost)
+
+}
+
+
+export const deletePost = async (req: Request, res: Response) => {
+    const post = await PostMessage.findById(req.params.postID)
+
+    if(!post){
+        res.status(400)
+        throw new Error("This post might just not exist")
+    }
+
+    const delPost = await PostMessage.findByIdAndRemove(req.params.postID)
+    res.status(200).json(delPost)
 }
