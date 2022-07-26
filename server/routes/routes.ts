660115f5a61d2b272d9909cea83becee849fb11b
ksowah <sowahkelvin640@gmail.com>
2022-07-26 23:32:48 +0000

    chain routes

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express"
-import { getPosts, createPost } from "../controllers/postsController"
+import { getPosts, createPost, updatePost, deletePost } from "../controllers/postsController"
 
 const router = express.Router()
 
-router.get("/", getPosts)
-router.post("/", createPost)
+router.route("/").get(getPosts).post(createPost)
+router.route("/:postID").put(updatePost).delete(deletePost)
 
 export default router
