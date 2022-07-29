793ad423f95a47bcd8c6a71ec12ee9c187eb7cbb
ksowah <sowahkelvin640@gmail.com>
2022-07-29 22:48:50 +0000

    add user to post schema

--- server/models/postModel.ts
+++ server/models/postModel.ts
@@ -13,6 +13,11 @@ const postSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: new Date()
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
     }
 })
 
