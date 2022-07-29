8c1a5d9c6610f98591d5d136bf962905ecdf4784
ksowah <sowahkelvin640@gmail.com>
2022-07-29 22:48:01 +0000

    build post controller

--- server/controllers/postsController.ts
+++ server/controllers/postsController.ts
@@ -1,49 +1,42 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import PostMessage from "../models/postModel"
 
 
-export const getPosts = async (req: Request, res: Response) => {
+export const getPosts = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const postMessages = await PostMessage.find()
         res.status(200).json(postMessages)
-    } catch (error: any) {
-        res.status(404).json({message: error.message})
+    } catch (error) {
+        next(error)
     }
 }
 
-export const createPost = async (req: Request, res: Response) => {
+export const createPost = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
         const post = await PostMessage.create(req.body)
         res.status(201).json(post)
    } catch (error) {
-    res.status(409).json({message: error})
+        next(error)
    }
 }
 
-export const updatePost = async (req: Request, res: Response) => {
-    const post = await PostMessage.findById(req.params.postID)
-
-    if(!post){
-        res.status(400)
-        throw new Error("Whoops!! couldn't find post in our database")
-    }
-
-    const updatePost = await PostMessage.findByIdAndUpdate(req.params.postID, req.body, { new: true })
-
-    res.status(200).json(updatePost)
-
+export const updatePost = async (req: Request, res: Response, next: NextFunction) => {
+   try {
+       const updatePost = await PostMessage.findByIdAndUpdate(req.params.postID, req.body, { new: true })
+       res.status(200).json(updatePost)
+   } catch (error) {
+    next(error)
+   }
 }
 
 
-export const deletePost = async (req: Request, res: Response) => {
-    const post = await PostMessage.findById(req.params.postID)
+export const deletePost = async (req: Request, res: Response, next: NextFunction) => {
+   try {
+       const delPost = await PostMessage.findByIdAndRemove(req.params.postID)
+       res.status(200).json(delPost)
+   } catch (error) {
+        next(error)
+   }
 
-    if(!post){
-        res.status(400)
-        throw new Error("This post might just not exist")
-    }
-
-    const delPost = await PostMessage.findByIdAndRemove(req.params.postID)
-    res.status(200).json(delPost)
 }
