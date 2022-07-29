0d8faf4c1d20d1734717d8b54266274c4386030c
ksowah <sowahkelvin640@gmail.com>
2022-07-29 22:47:41 +0000

    add user routes

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,9 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import connectDB from "./config/db"
-import router from "./routes/routes"
+import postRouter from "./routes/postRoutes.routes"
+import errorHandler from "./middleware/errorHandler"
+import userRouter from "./routes/userRoutes.routes"
 
 dotenv.config()
 
@@ -12,7 +14,11 @@ const app  = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
-app.use("/posts", router)
+app.use("/posts", postRouter)
+app.use("/users", userRouter)
+app.use(errorHandler)
+
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`⛈️ ⚡ server running on port ${PORT}`))
