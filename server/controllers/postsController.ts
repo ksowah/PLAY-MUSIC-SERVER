bc38744c6050792608beb11be2eace6f8d5e9cdc
ksowah <sowahkelvin640@gmail.com>
2022-08-02 23:46:31 +0000

    some checks

--- server/controllers/postsController.ts
+++ server/controllers/postsController.ts
@@ -6,6 +6,8 @@ export const getPosts = async (req: Request, res: Response, next: NextFunction) 
     try {
         const postMessages = await PostMessage.find()
         res.status(200).json(postMessages)
+        console.log(req.headers["authorization"]);
+        
     } catch (error) {
         next(error)
     }
