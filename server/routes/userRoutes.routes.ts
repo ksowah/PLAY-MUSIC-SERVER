ae593e1bbd32c841b8307e8793a304f454fe3aee
ksowah <sowahkelvin640@gmail.com>
2022-08-11 18:23:18 +0000

    protect users route

--- server/routes/userRoutes.routes.ts
+++ server/routes/userRoutes.routes.ts
@@ -1,9 +1,10 @@
 import express, { Express } from "express";
 import { getUsers, loginUser, registerUser } from "../controllers/userController";
+import protect from "../middleware/authMiddleware";
 
 const userRouter = express.Router()
 
-userRouter.route("/").get(getUsers).post(registerUser)
+userRouter.route("/").get(protect, getUsers).post(registerUser)
 userRouter.route("/login").post(loginUser)
 
 export default userRouter
