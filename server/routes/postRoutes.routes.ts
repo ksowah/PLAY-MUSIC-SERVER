0e13c960153f658da1bd0b1d3751600bd5e4c39c
ksowah <sowahkelvin640@gmail.com>
2022-08-11 18:22:58 +0000

    protect post route

--- server/routes/postRoutes.routes.ts
+++ server/routes/postRoutes.routes.ts
@@ -5,6 +5,6 @@ import protect from "../middleware/authMiddleware"
 const postRouter = express.Router()
 
 postRouter.route("/").get(protect, getPosts).post(protect, createPost)
-postRouter.route("/:postID").put(protect, updatePost).delete(deletePost)
+postRouter.route("/:postID").put(protect, updatePost).delete( protect, deletePost)
 
 export default postRouter
