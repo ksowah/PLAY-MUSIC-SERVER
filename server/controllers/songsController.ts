76d394a72d3fdd94737b6215cd237236b6f4b7d9
ksowah <sowahkelvin640@gmail.com>
2022-08-16 23:42:56 +0000

    create song controller

---
+++ server/controllers/songsController.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express"
+import TrackModel from "../models/trackModel"
+
+
+export const getSongs = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const getTracks = await TrackModel.find({user: req.user.email})
+        res.status(200).json(getTracks)
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const addSong = async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const song = await TrackModel.create(req.body)
+        res.status(201).json(song)
+   } catch (error) {
+        next(error)
+   }
+}
+
+// export const updateSong = async (req: Request, res: Response, next: NextFunction) => {
+//    try {
+//        const updatePost = await TrackModel.findByIdAndUpdate(req.params.postID, req.body, { new: true })
+//        res.status(200).json(updatePost)
+//    } catch (error) {
+//     next(error)
+//    }
+// }
+
+
+export const deleteSong = async (req: Request, res: Response, next: NextFunction) => {
+   try {
+       const delSong = await TrackModel.findByIdAndRemove(req.params.songID)
+       res.status(200).json(delSong)
+   } catch (error) {
+        next(error)
+   }
+
+}
