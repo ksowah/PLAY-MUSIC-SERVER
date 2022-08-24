6124cdd6577562796e6ad649fb3eee988366389a
ksowah <sowahkelvin640@gmail.com>
2022-08-24 22:32:13 +0000

    soe changes

--- middleware/corsOrigin.ts
+++ middleware/corsOrigin.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 const corsOrigin = (req: Request, res: Response, next: NextFunction) => {
-        res.header("Access-Control-Allow-Origin", "true"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "true");
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Vary", "origin");
         next();
 }
 
