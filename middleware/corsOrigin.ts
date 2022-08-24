500f2126b58ef4b663797eb1e12a5c036bcf8577
ksowah <sowahkelvin640@gmail.com>
2022-08-24 21:55:27 +0000

    fixes

--- middleware/corsOrigin.ts
+++ middleware/corsOrigin.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 const corsOrigin = (req: Request, res: Response, next: NextFunction) => {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Origin", "true"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "true");
         next();
 }
 
